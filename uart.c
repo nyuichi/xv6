95eb1055f4dc8a8870ba25372ef1f0f80fd73096
m-hys <mh.northlander@gmail.com>
2015-02-14 00:05:38 +0900

    uartinit

--- uart.c
+++ uart.c
@@ -19,34 +19,11 @@ void
 uartinit(void)
 {
   char *p;
-  /*
-  // Turn off the FIFO
-  outb(COM1+2, 0);
-
-  // 9600 baud, 8 data bits, 1 stop bit, parity off.
-  outb(COM1+3, 0x80);    // Unlock divisor
-  outb(COM1+0, 115200/9600);
-  outb(COM1+1, 0);
-  outb(COM1+3, 0x03);    // Lock divisor, 8 data bits.
-  outb(COM1+4, 0);
-  outb(COM1+1, 0x01);    // Enable receive interrupts.
-
-  // If status is 0xFF, no serial port.
-  if(inb(COM1+5) == 0xFF)
-    return;
   uart = 1;
-
-  // Acknowledge pre-existing interrupt conditions;
-  // enable interrupts.
-  inb(COM1+2);
-  inb(COM1+0);
-  picenable(IRQ_COM1);
-  */
   // Announce that we're here.
-  uart = 1;
-
-  for(p="xv6...\n"; *p; p++)
+  for(p="xv6...\n"; *p; p++){
     uartputc(*p);
+  }
 }
 
 void
