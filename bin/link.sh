de798cad092a723e0fbba0f27ac2c0581b6cfb19
nullpo-head <abc.tkys+pub@gmail.com>
2015-02-19 05:29:03 +0900

    delete bin directory

--- bin/link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dir=`dirname "$0"`
-rm -f $dir/*.out*
-$dir/../../ucc/bin/as  -o $dir/xv6.out $dir/*.s $dir/../trapasm.S $dir/../swtch.S -l $dir/../../ucc/lib/libucc.s -s -f __UCC_HEAP_START
