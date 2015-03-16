27ca0d4935a8c57089b690f190ef833a6dc7d3c6
Wataru Inariba <oinari17@gmail.com>
2015-03-16 17:26:26 +0900

    append simple readline library to shell

--- usr/sh.c
+++ usr/sh.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <termios.h>
 
 // Parsed command representation
 #define EXEC  1
@@ -132,21 +133,13 @@ runcmd(struct cmd *cmd)
   exit(0);
 }
 
-int
-getcmd(char *buf, int nbuf)
-{
-  fprintf(stderr, "$ ");
-  memset(buf, 0, nbuf);
-  fgets(buf, nbuf, stdin);
-  if(buf[0] == 0) // EOF
-    return -1;
-  return 0;
-}
+char *readline();
+void free_history();
 
 int
 main(void)
 {
-  static char buf[100];
+  char *buf;
   int fd;
 
   // Assumes three file descriptors open.
@@ -158,7 +151,7 @@ main(void)
   }
 
   // Read and run input commands.
-  while(getcmd(buf, sizeof(buf)) >= 0){
+  while((buf = readline())){
     if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
       // Clumsy but will have to do for now.
       // Chdir has no effect on the parent if run in the child.
@@ -171,6 +164,8 @@ main(void)
       runcmd(parsecmd(buf));
     wait();
   }
+
+  free_history();
   exit(0);
 }
 
@@ -492,3 +487,245 @@ nulterminate(struct cmd *cmd)
   }
   return cmd;
 }
+
+
+/* readline library */
+
+#define PROMPT      "$ "
+#define PROMPT_LEN   2
+#define HISTORY     16
+#define TMP_UNIT    64
+#define REALLOC(p, size) \
+  do if (! (p = realloc(p, size))) { fprintf(stderr, "realloc failed\n"); exit(1); } while (0)
+
+char *hist[HISTORY], *tmp;
+int hist_index, cur_index;
+int tmp_size;
+int pos, len, col = 80;
+
+void extend_tmp()
+{
+  tmp_size += TMP_UNIT;
+  REALLOC(tmp, tmp_size + 1);
+}
+
+void move_cursor(int target, int opt)
+{
+  int cx = ((opt ? len : pos) + PROMPT_LEN) % col;
+  int cy = ((opt ? len : pos) + PROMPT_LEN) / col;
+  int tx = (target + PROMPT_LEN) % col;
+  int ty = (target + PROMPT_LEN) / col;
+  if (ty < cy) printf("\x1b[%dA", cy - ty);
+  if (ty > cy) printf("\x1b[%dB", ty - cy);
+  if (tx > cx) printf("\x1b[%dC", tx - cx);
+  if (tx < cx) printf("\x1b[%dD", cx - tx);
+  pos = target;
+}
+
+void key_left()  { if (pos > 0)   move_cursor(pos - 1, 0); }
+void key_right() { if (pos < len) move_cursor(pos + 1, 0); }
+void key_home()  { move_cursor(0, 0); }
+void key_end()   { move_cursor(len, 0); }
+
+void move_history(int idx)
+{
+  cur_index = idx;
+  key_home();
+  pos = len;
+  memset(tmp, ' ', len);
+  printf("%s", tmp);
+  if ((len + PROMPT_LEN) % col == 0)
+    printf("\n");
+  key_home();
+  if (idx == hist_index) {
+    len = 0;
+    tmp[0] = '\0';
+    return;
+  }
+  pos = len = strlen(hist[idx]);
+  if (tmp_size < len) {
+    tmp_size = (len / TMP_UNIT + 1) * TMP_UNIT;
+    REALLOC(tmp, tmp_size + 1);
+  }
+  memcpy(tmp, hist[idx], len + 1);
+  printf("%s", tmp);
+  if ((len + PROMPT_LEN) % col == 0)
+    printf("\n");
+}
+
+void key_up()
+{
+  int idx = (cur_index - 1 + HISTORY) % HISTORY;
+  if (idx == hist_index || hist[idx] == NULL)
+    return;
+  move_history(idx);
+}
+
+void key_down()
+{
+  if (cur_index == hist_index)
+    return;
+  move_history((cur_index + 1) % HISTORY);
+}
+
+void key_delete()
+{
+  int i;
+  if (pos == len)
+    return;
+  for (i = pos; i < len; ++i)
+    tmp[i] = tmp[i + 1];
+  printf("%s ", tmp + pos);
+  if ((len + PROMPT_LEN) % col == 0)
+    printf("\n");
+  move_cursor(pos, 1);
+  --len;
+}
+
+void key_backspace()
+{
+  if (pos == 0)
+    return;
+  key_left();
+  key_delete();
+}
+
+void key_enter()
+{
+  int prv;
+  key_end();
+  printf("\n");
+  while (len > 0 && tmp[len - 1] == ' ')
+    tmp[--len] = '\0';
+  prv = (hist_index - 1 + HISTORY) % HISTORY;
+  if (len == 0 || tmp[0] == ' ' || (hist[prv] && !strcmp(tmp, hist[prv])))
+    return;
+  REALLOC(hist[hist_index], len + 1);
+  memcpy(hist[hist_index], tmp, len + 1);
+  hist_index = (hist_index + 1) % HISTORY;
+}
+
+void insert_char(int c)
+{
+  int i;
+  if (len == tmp_size)
+    extend_tmp();
+  for (i = len; i >= pos; --i)
+    tmp[i + 1] = tmp[i];
+  tmp[pos] = c;
+  printf("%s", tmp + pos);
+  ++pos; ++len;
+  if ((len + PROMPT_LEN) % col == 0)
+    printf("\n");
+  move_cursor(pos, 1);
+}
+
+// returned pointer is valid until next readline call
+char *readline()
+{
+  static int init_term = 0;
+  static struct termios original, settings;
+
+  int state = 0, ctrl_d = 0;
+
+  tmp_size = TMP_UNIT;
+  REALLOC(tmp, tmp_size + 1);
+
+  printf(PROMPT);
+  fflush(stdout);
+
+  if (!init_term) {
+    init_term = 1;
+    tcgetattr(0, &original);
+    settings = original;
+    cfmakeraw(&settings);
+  }
+  tcsetattr(0, TCSANOW, &settings);
+
+  tmp[0] = '\0';
+  pos = len = 0;
+  cur_index = hist_index;
+
+  while (1) {
+    char c;
+    read(0, &c, 1);
+    if (c == '\x1b') {
+      state = 1;
+    } else if (state == 1) {    // \x1b
+      state = 0;
+      if (c == '[') state = 2;
+      if (c == 'O') state = 3;
+    } else if (state == 2) {    // \x1b[
+      switch (c) {
+        case 'A': state = 0; key_up();    break;
+        case 'B': state = 0; key_down();  break;
+        case 'C': state = 0; key_right(); break;
+        case 'D': state = 0; key_left();  break;
+        case '1': state = 4; break;
+        case '3': state = 5; break;
+        case '4': state = 6; break;
+        case '2': case '5': case '6': state = 7; break;
+        default:  state = 0; break;
+      }
+    } else if (state == 3) {    // \x1bO
+      state = 0;
+      if (c == 'H') key_home();
+      if (c == 'F') key_end();
+    } else if (state == 4) {    // \x1b[1
+      state = 0;
+      if (c == '~') key_home();
+      if (c == ';') state = 8;
+    } else if (state == 5) {    // \x1b[3
+      state = 0;
+      if (c == '~') key_delete();
+    } else if (state == 6) {    // \x1b[4
+      state = 0;
+      if (c == '~') key_end();
+    } else if (state == 7) {
+      state = 0;
+    } else if (state == 8) {    // \x1b[1;
+      state = c == '5' ? 9 : 0;
+    } else if (state == 9) {    // \x1b[1;5
+      state = 0;
+      if (c == 'C') key_end();
+      if (c == 'D') key_home();
+    } else if (c == 127) {
+      key_backspace();
+    } else if (c < 32) {
+      if (c == 10 || c == 13) { key_enter(); break; }
+      if (c == 4) {
+        if (! len) { ctrl_d = 1; break; }
+        else key_delete();
+      }
+      if (c ==  1) key_home();
+      if (c ==  2) key_left();
+      if (c ==  5) key_end();
+      if (c ==  6) key_right();
+      if (c ==  8) key_backspace();
+      if (c == 14) key_down();
+      if (c == 16) key_up();
+    } else {
+      insert_char(c);
+    }
+    fflush(stdout);
+  }
+
+  tcsetattr(0, TCSANOW, &original);
+
+  if (ctrl_d) {
+    printf("\n");
+    fflush(stdout);
+    return NULL;
+  }
+
+  fflush(stdout);
+  return tmp;
+}
+
+void free_history() {
+  int i;
+  for (i = 0; i < HISTORY; ++i)
+    free(hist[i]);
+  free(tmp);
+}
+
